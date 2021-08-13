f452c835b54f42531d43449e850a927ad7f7a25a
ghadooo <62287731+ghadooo@users.noreply.github.com>
2021-08-13 12:34:51 +0100

    Update ethernet_server.cc

--- ethernet_server.cc
+++ ethernet_server.cc
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector> 
 
+#include <grpcpp/grpcpp.h>
+#include "viface/viface.hpp"
+
+#ifdef BAZEL_BUILD    //not sure if this is the right format
+
+#include "etherInter.grpc.pb.h"
+#endif
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using etherInter::InfoRequest;
+using etherInter::InfoReply;
+using etherInter::Interface;
+using etherInter::State;
+using etherInter::InterfaceManager;
+
+// Logic and data behind the server's behavior.
+class InterfaceManagerServiceImpl final : public InterfaceManager::Service {
+  Status getInformation(ServerContext* context, const InfoRequest* request,
+                  InfoReply* reply) override {
+    vector<std::string> prefix(4);
+    prefix(0)="Name";
+    prefix(1)="IPV4";
+    prefix(2)="DHCP";
+    prefix(3)="State";
+    vector<std::string> reply(4);
+    for (i(0); i<4 ; ++i) {
+    reply(i)->set_message(prefix(i) + request->name()); }
+    return Status::OK;
+  }
+};
