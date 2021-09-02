bd44944d535459bf71ff22827fae449540e754ed
Talel Bel Hadj Salem <talel.hajsalem@sofia-technologies.com>
2021-09-02 20:27:50 +0100

    Add Makefile type

---
+++ Makefile/ethernet_client.cc
@@ -0,0 +1,221 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector> 
+
+#include <grpcpp/grpcpp.h>
+#include "viface/viface.hpp"
+
+#include "etherInter.grpc.pb.h"
+#include "etherInter.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using ethernet::InterfaceManager;
+using ethernet::InfoRequest;
+using ethernet::InfoReply;
+using ethernet::setIPV4Request;
+using ethernet::setDHCPRequest;
+using ethernet::setStateRequest;
+using ethernet::Empty;
+
+class InterfaceManagerClient  {
+ public:
+ 
+   InterfaceManagerClient(std::shared_ptr<Channel> channel)
+      : stub_(InterfaceManager::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.     
+   void getInformation(std::string name) {
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    //std::cout << "Context created. \n";
+
+    // Data we are sending to the server.
+    InfoRequest request;
+    request.set_name(name);
+    //std::cout << "Request created. \n";
+
+    // Container for the data we expect from the server.
+    InfoReply reply;
+    //std::cout << "Reply created. \n";
+
+    // The actual RPC.
+    Status status = stub_->getInformation(&context, request, &reply);
+    //std::cout << "getInformation sent. \n";
+
+    // Act upon its status.
+    if (status.ok()) {
+      std::cout << "Status ok \n";
+      const char * state = (reply.state() == true) ? "Up" : "Down";
+      
+      std::cout << "+------ " << name << " information ------+ \n";
+      std::cout << "|\tMAC  : " << reply.mac() << "\t|\n";
+      std::cout << "|\tIPv4 : " << reply.ip() << "\t|\n";
+      std::cout << "|\t MASK : " << reply.mask() << "\t|\n";
+      std::cout << "|\t STATE: " << state << "\t|\n";
+      std::cout << "+--------------------------------+ \n";
+
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }
+  }   
+
+
+void setIPV4(std::string name, std::string ip, std::string mask) {
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    //std::cout << "Context created. \n";
+
+    // Data we are sending to the server.
+    setIPV4Request request;
+    request.set_name(name);
+    request.set_ip(ip);
+    request.set_mask(mask);
+    //std::cout << "Request created. \n";
+
+    // Container for the data we expect from the server.
+    Empty reply;
+    //std::cout << "Reply created. \n";
+
+    // The actual RPC.
+    Status status = stub_->setIPV4(&context, request, &reply);
+    //std::cout << "getInformation sent. \n";
+
+    // Act upon its status.
+    if (status.ok()) {
+      std::cout << "Status ok \n";
+      
+      std::cout << "+------ Setting " << name << "'s IPv4 ------+ \n";
+      //std::cout << "|\t MAC  : " << reply.mac() << "\t|\n";
+      std::cout << "|\t new IPv4 : " << ip << "\t|\n";
+      std::cout << "|\t new MASK : " << mask << "\t|\n";
+      //std::cout << "|\t STATE: " << state << "\t|\n";
+      std::cout << "+--------------------------------+ \n";
+
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }
+  }   
+
+void setState(std::string name, bool new_state) {
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+    //std::cout << "Context created. \n";
+
+    // Data we are sending to the server.
+    setStateRequest request;
+    request.set_name(name);
+    request.set_state(new_state);
+ 
+    //std::cout << "Request created. \n";
+
+    // Container for the data we expect from the server.
+    Empty reply;
+    //std::cout << "Reply created. \n";
+
+    // The actual RPC.
+    Status status = stub_->setState(&context, request, &reply);
+    //std::cout << "setState sent. \n";
+
+    // Act upon its status.
+    if (status.ok()) {
+      std::cout << "Status ok \n";
+      
+      std::cout << "+------ Setting " << name << "'s state ------+ \n";
+      //std::cout << "|\t MAC  : " << reply.mac() << "\t|\n";
+      //std::cout << "|\t new IPv4 : " << reply.ip() << "\t|\n";
+      //std::cout << "|\t new MASK : " << reply.mask() << "\t|\n";
+      std::cout << "|\t new STATE: " << new_state << "\t|\n";
+      std::cout << "+--------------------------------+ \n";
+
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }
+  }
+
+
+  
+  private:
+   std::unique_ptr<InterfaceManager::Stub> stub_;  
+};
+
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // localhost at port 50051). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  InterfaceManagerClient infoClient(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+  //std::cout << "Client created. \n";
+
+  infoClient.getInformation("wlp3s0");
+  //infoClient.getInformation("enp2s0");
+
+  std::cout << "+------ Further Settings  ------+ \n";
+  std::cout << "|\t Setting IPv4    : 1\t|\n";
+  std::cout << "|\t Setting state   : 2\t|\n";
+  std::cout << "|\t Setting DHCP    : 3\t|\n";
+  std::cout << "|\t EXIT            : 4\t|\n";
+  std::cout << "+--------------------------------+ \n";
+
+  int x;
+  std::cin >> x ;
+  switch (x)
+  {
+  case 1:
+    {
+      std::string new_ip, new_mask;
+      std::cout << "\n Please enter new IPv4 : \t";
+      std::cin >> new_ip;
+      std::cout << "\n Please enter new mask : \t";
+      std::cin >> new_ip;
+      std::cout << "\n\n"; 
+      infoClient.setIPV4("wlp1s0", new_ip, new_mask);
+      break;
+    }
+    
+  case 2:
+      {
+        bool new_state;
+        std::cout << "\n Please enter new state : \t";
+        std::cin >> new_state;
+        std::cout << "\n\n"; 
+        infoClient.setState("wlp1s0", new_state);
+        break;
+      }
+
+  case 3:
+      {
+        std::string new_dhcp;
+        std::cout << "\n Please enter new DHCP : \t";
+        std::cin >> new_dhcp;
+        std::cout << "\n\n"; 
+        break;
+      }
+
+  case 4:
+      {
+        std::cout << "\n Exiting.....\t";
+        sleep(1);
+        exit(EXIT_SUCCESS);
+        break;
+      }
+
+  default:
+    std::cout << " Invalid choice! \n";
+    break;
+  }
+
+  return 0;
+}
---
+++ Makefile/ethernet_server.cc
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "viface/viface.hpp"
+
+#include "etherInter.grpc.pb.h"
+#include "etherInter.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using ethernet::InterfaceManager;
+using ethernet::InfoRequest;
+using ethernet::InfoReply;
+using ethernet::setIPV4Request;
+using ethernet::setDHCPRequest;
+using ethernet::setStateRequest;
+using ethernet::Empty;
+
+// Logic and data behind the server's behavior.
+class InterfaceManagerServiceImpl final : public InterfaceManager::Service {
+  
+  // displays network information
+  Status getInformation(
+                  ServerContext* context, 
+                  const InfoRequest* request,
+                  InfoReply* reply) override {
+
+    // libviface
+    std::cout << "Got request for getInformation, interface: " << request->name() << "\n";
+    viface::VIface iface(request->name());
+
+    reply->set_mac(iface.getMAC());
+    reply->set_ip(iface.getIPv4());
+    reply->set_mask(iface.getIPv4Netmask());
+    reply->set_state(iface.isUp());
+
+    return Status::OK;
+  }
+
+    // sets the IPV4 
+    Status setIPV4(
+                  ServerContext* context, 
+                  const setIPV4Request* request, 
+                  Empty* response) override {
+      // libviface
+      std::cout << "Got request for setIPV4, interface: " << request->name() << "\n";
+      viface::VIface iface(request->name());
+
+      iface.setIPv4(request->ip());
+      iface.setIPv4Netmask(request->mask());
+
+      return Status::OK;
+    }
+
+
+    // sets the network DHCP 
+    Status setDHCP(
+                  ServerContext* context, 
+                  const setDHCPRequest* request, 
+                  Empty* response) override {
+      // libviface
+      std::cout << "Got request for setDHCP, interface: " << request->name() << "\n";
+      viface::VIface iface(request->name());
+
+      // looking for DHCP in lib
+
+      return Status::OK;
+    }
+
+
+    // sets the network state    
+    Status setState(
+                  ServerContext* context, 
+                  const setStateRequest* request, 
+                  Empty* response) override {
+      // libviface
+      std::cout << "Got request for setState, interface: " << request->name() << "\n";
+      viface::VIface iface(request->name());
+
+      if (request->state()){
+        iface.up();
+      }
+      else iface.down();
+      
+
+      return Status::OK;
+    }
+
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  InterfaceManagerServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+  return 0;
+}
